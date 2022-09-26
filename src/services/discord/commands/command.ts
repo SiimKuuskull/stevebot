7f31c5aeaec0803d9abf5de2819d00c9dcf4b2d7
Luukuduja <siim_k@outlook.com>
2022-09-26 13:07:17 +0300

    added moomincoin command

--- src/services/discord/commands/command.ts
+++ src/services/discord/commands/command.ts
@@ -1,6 +1,7 @@
 import { helpCommand } from './help/help';
+import { moominCoin } from './moomincoin/moomincoin';
 import { myBalance } from './my-balance/my-balance';
 import { myBet } from './my-bet/my-bet';
 import { placeBet } from './place-bet/placeBet';
 
-export const commands = [helpCommand, myBalance, placeBet, myBet];
+export const commands = [helpCommand, myBalance, placeBet, myBet, moominCoin];
