01cc0da5844b9eb5394c49da88fd6c03a7b6f599
Luukuduja <siim_k@outlook.com>
2022-10-03 23:59:05 +0300

    Added 'bet-history' command to get user's past bets and the user's profit

---
+++ src/services/discord/commands/bet-history/bet-history.ts
@@ -0,0 +1,52 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { db } from '../../../../database/db';
+import { Bet, BetGuess, BetResult } from '../../../../database/models/bet.model';
+import { log } from '../../../../tools/logger';
+
+export const betHistory = {
+    data: new SlashCommandBuilder().setName('bet-history').setDescription('Check bet history'),
+    execute: async (interaction) => {
+        const bets = await getUserBets(interaction.user.id);
+        const profit = await getUserProfit(interaction.user.tag, bets);
+        let index = 0;
+        await interaction.reply({
+            content: `${interaction.user.tag}  panused:\n
+            Kogus:          Koefitsent:          Pakkumine:              Tulemus:
+        ${bets
+            .map((bet) => {
+                const result = `
+${(index += 1)}.                    ${bet.amount}              ${bet.odds}                         ${
+                    bet.guess
+                }                         ${bet.result} \n`;
+                return `${result}`;
+            })
+            .toString()
+            .replaceAll(',', '')}
+${interaction.user.tag} kasum on ${profit} muumimünti      `,
+            ephemeral: true,
+        });
+    },
+};
+
+async function getUserBets(userId) {
+    const bets = await db<Bet>('bets').select().where('userId', userId);
+    log(bets);
+    return bets;
+}
+async function getUserProfit(user, bets) {
+    let profit = 0;
+    bets.forEach((bet: Bet) => {
+        if (bet.guess === BetGuess.WIN && bet.result === BetResult.WIN) {
+            profit += bet.amount * bet.odds;
+        }
+        if (
+            (bet.guess === BetGuess.WIN && bet.result === BetResult.LOSE) ||
+            (bet.guess === BetGuess.LOSE && bet.result === BetResult.WIN)
+        ) {
+            profit -= bet.amount * bet.odds;
+        }
+        return profit;
+    });
+    log(`User: ${user} profits ${profit} muumicoins`);
+    return profit;
+}
--- src/services/discord/commands/command.ts
+++ src/services/discord/commands/command.ts
@@ -1,7 +1,8 @@
+import { betHistory } from './bet-history/bet-history';
 import { helpCommand } from './help/help';
 import { leaderboard } from './leaderboard/leaderboard';
 import { myBalance } from './my-balance/my-balance';
 import { myBet } from './my-bet/my-bet';
 import { placeBet } from './place-bet/placeBet';
 
-export const commands = [helpCommand, myBalance, placeBet, myBet, leaderboard];
+export const commands = [helpCommand, myBalance, placeBet, myBet, leaderboard, betHistory];
