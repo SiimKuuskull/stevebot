a104960efdba8e1985e02cbfadbb4d568418e4cf
Mihkel Kuusküll <mihkel.kuuskull@gmail.com>
2022-10-07 00:49:13 +0300

    Fixed tests + can now test interaction replies

--- test/integration/commands/my-balance.test.ts
+++ test/integration/commands/my-balance.test.ts
@@ -1,4 +1,4 @@
-import { testDb } from '../init';
+import { sandbox, testDb } from '../init';
 import { myBalance } from '../../../src/services/discord/commands/my-balance/my-balance';
 import { getTestBalanceTemplate, getTestInteraction } from '../../test-data';
 import { expect } from 'chai';
@@ -6,16 +6,36 @@ import { createUserBalance } from '../../../src/database/queries/balance.query';
 
 describe('Discord command - /my-balance', () => {
     const { execute } = myBalance;
-    it('Should return existing balance', async () => {
+    it.only('Should return existing balance', async () => {
+        const interaction = getTestInteraction();
+        const spy = sandbox.spy(interaction, 'reply');
         const existingBalance = await createUserBalance(getTestBalanceTemplate());
-        await execute(getTestInteraction());
+
+        await execute(interaction);
+
         const balances = await testDb('balance');
         expect(balances.length).to.eq(1);
         expect(existingBalance).to.deep.equal(balances[0]);
+        expect(
+            spy.calledWith({
+                content: `Sul on 100 muumimünti`,
+                ephemeral: true,
+            }),
+        ).to.eq(true);
     });
     it('Should create new balance for user', async () => {
-        await execute(getTestInteraction());
+        const interaction = getTestInteraction();
+        const spy = sandbox.spy(interaction, 'reply');
+
+        await execute(interaction);
+
         const balances = await testDb('balance');
         expect(balances.length).to.eq(1);
+        expect(
+            spy.calledWith({
+                content: `Sul on 100 muumimünti`,
+                ephemeral: true,
+            }),
+        ).to.eq(true);
     });
 });
