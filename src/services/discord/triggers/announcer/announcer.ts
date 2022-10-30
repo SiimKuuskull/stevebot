c356dd9b11f14ec7f43d9564efa3d678b1ae9343
Mihkel Kuusküll <mihkel.kuuskull@gmail.com>
2022-10-30 19:45:29 +0200

    Simplified announcer trigger

--- src/services/discord/triggers/announcer/announcer.ts
+++ src/services/discord/triggers/announcer/announcer.ts
@@ -4,11 +4,9 @@ import {
     findSteveGameById,
     updateSteveGame,
 } from '../../../../database/queries/steveGames.query';
-import { SteveGameStatus } from '../../../../database/models/steveGame.model';
 import { sendChannelMessage } from '../../utils';
 import { findTrackedPlayer } from '../../../../database/queries/player.query';
 import { getActiveLeagueGame, getLatestFinishedLeagueGame } from '../../../game.service';
-import { log } from '../../../../tools/logger';
 
 export const announcer = {
     interval: 10,
@@ -27,29 +25,20 @@ export const announcer = {
         }
         const finishedGameId = await getLatestFinishedLeagueGame(player.puuid);
         const existingGame = await findSteveGameById(game.gameId.toString());
-        if (!existingGame) {
-            if (finishedGameId !== game.gameId.toString()) {
-                if (game.gameStartTime !== 0) {
-                    await createSteveGame({
-                        gameId: game.gameId.toString(),
-                        gameStart: game.gameStartTime,
-                        gameStatus: SteveGameStatus.IN_PROGRESS,
-                    });
-                }
-                if (!game.gameStartTime || game.gameStartTime === 0) {
-                    await createSteveGame({
-                        gameId: game.gameId.toString(),
-                        gameStart: Date.now(),
-                        gameStatus: SteveGameStatus.IN_PROGRESS,
-                    });
-                }
-                const gameMode = {
-                    CLASSIC: 'normal',
-                    RANKED: 'ranked',
-                    ARAM: 'aram',
-                };
-                sendChannelMessage(`${player.name} läks just uude ${gameMode[game.gameMode]} mängu`);
-            }
+        const isNewGame = !existingGame && finishedGameId !== game.gameId.toString();
+        if (!isNewGame) {
+            return;
         }
+        await createSteveGame({
+            gameId: game.gameId.toString(),
+            gameStart: game.gameStartTime || Date.now(),
+        });
+        sendChannelMessage(`${player.name} läks just uude ${gameMode[game.gameMode] || 'featured gamemode'} mängu`);
     },
+};
+
+const gameMode = {
+    CLASSIC: 'normal',
+    RANKED: 'ranked',
+    ARAM: 'aram',
 };
