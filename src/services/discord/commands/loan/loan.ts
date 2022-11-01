4eccca4c017bdb8b90155ccb41e7f307529354e9
Luukuduja <siim_k@outlook.com>
2022-11-01 23:15:08 +0200

    loan-test, loan input missing

--- src/services/discord/commands/loan/loan.ts
+++ src/services/discord/commands/loan/loan.ts
@@ -15,7 +15,6 @@ export const loan = {
         .setDescription('Laena!')
         .addIntegerOption((option) => option.setName('loan-number').setDescription('summa').setRequired(true)),
     execute: async (interaction) => {
-        log(interaction.options.getInteger('loan-number'));
         const balance = await findUserBalance(interaction.user.id);
         if (!balance) {
             log(`No active balance found.`);
@@ -26,19 +25,19 @@ export const loan = {
             });
         }
         if (balance) {
-            log(interaction.options.getInteger('loan-number'));
-            const loanInput = interaction.options.getInteger('loan-number');
             const bankruptCount = await getBankruptcyCount(interaction.user.id);
             const [existingLoan] = await findUserActiveLoan(interaction.user.id);
-            if (bankruptCount >= 5 || existingLoan?.payback === LoanPayBack.RESOLVED) {
+            if (bankruptCount >= 5 || existingLoan?.payback === LoanPayBack.UNRESOLVED) {
                 await interaction.reply({
                     content: `Suur Muum ei rahulda su laenusoovi ja soovitab majandusliku abi otsida mujalt`,
                     components: [],
                     ephemeral: true,
                 });
                 log(`${interaction.user.tag} has reached bankruptcy limit: ${bankruptCount} times. No actions taken.`);
+                return;
             }
-            if (bankruptCount < 5 && loanInput) {
+            if (bankruptCount < 5) {
+                const loanInput = interaction.options.getInteger('loan-number');
                 if (loanInput >= 3000) {
                     await interaction.reply({
                         content: `Suur Muum ei saa sinule nii suurt laenu pakkuda. Proovi laenata vähem kui 3000 muumimünti!`,
---
+++ test/integration/commands/loan.test.ts
@@ -0,0 +1,78 @@
+import { expect } from 'chai';
+import { LoanPayBack } from '../../../src/database/models/loan.model';
+import { createUserBalance } from '../../../src/database/queries/balance.query';
+import { createLoan } from '../../../src/database/queries/loans.query';
+import { loan } from '../../../src/services/discord/commands/loan/loan';
+import { enableLogs } from '../../../src/tools/logger';
+import {
+    getTestBalanceTemplate,
+    getTestInteraction,
+    getUnresolvedTestLoanTemplate,
+    TEST_DISCORD_USER,
+} from '../../test-data';
+import { sandbox, testDb } from '../init';
+
+describe('Discord command - /loan', () => {
+    const { execute } = loan;
+    it('Should create a balance if no balance is found.', async () => {
+        const interaction = getTestInteraction();
+        const spy = sandbox.spy(interaction, 'reply');
+
+        await execute(interaction);
+
+        const balances = await testDb('balance');
+        expect(balances.length).to.eq(1);
+        expect(spy.args[0][0]).to.deep.equal({
+            content: `Ei leidnud sinu nimel aktiivset kontot. Seega saad 100 muumimünti enda uuele kontole. GL!`,
+            ephemeral: true,
+        });
+    });
+    it('Should not give out a loan if the bankruptcy count is >= 5 or there is an unresolved loan', async () => {
+        const interaction = getTestInteraction();
+        const spy = sandbox.spy(interaction, 'reply');
+        const balance = await createUserBalance(getTestBalanceTemplate({ amount: 100, bankruptcy: 5 }));
+        await createLoan(getUnresolvedTestLoanTemplate());
+
+        await execute(interaction);
+
+        const balances = await testDb('balance');
+        const loans = await testDb('loans').whereNot({ payback: LoanPayBack.UNRESOLVED });
+        expect(loans.length).to.eq(0);
+        expect(balances.length).to.eq(1);
+        expect(balance.bankruptcy).to.greaterThanOrEqual(5);
+        expect(spy.args[0][0]).to.deep.equal({
+            content: `Suur Muum ei rahulda su laenusoovi ja soovitab majandusliku abi otsida mujalt`,
+            components: [],
+            ephemeral: true,
+        });
+    });
+    it('Should not give out a loan if the requested amount is greater than set limit', async () => {
+        const interaction = getTestInteraction();
+        const spy = sandbox.spy(interaction, 'reply');
+        const balance = await createUserBalance(getTestBalanceTemplate({ amount: 100 }));
+
+        await execute(interaction);
+    });
+    it('Should give out a loan if the bankruptcy count is less than 5, and the amount is below the limit', async () => {
+        const loanInput = 1000;
+        const interaction = getTestInteraction({ option: loanInput });
+        const spy = sandbox.spy(interaction, 'reply');
+        const balance = await createUserBalance(getTestBalanceTemplate({ amount: 1000, bankruptcy: 0 }));
+
+        await execute(interaction);
+        
+        expect(spy.calledOnce).to.eq(true);
+        expect(spy.args[0][0]).to.deep.equal({
+            content: `${TEST_DISCORD_USER.tag} sai 1000 laenu intressiga ${
+                0.08 * 100
+            }%, tagasimakse aeg on ${Date.now()}`,
+            ephemeral: true,
+        });
+
+        const balances = await testDb('balance');
+        const loans = await testDb('loans');
+        expect(loans.length).to.eq(1);
+        expect(balances.length).to.eq(1);
+        expect(balance.bankruptcy).to.lessThanOrEqual(5);
+    });
+});
