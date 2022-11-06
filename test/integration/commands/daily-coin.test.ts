7a7588cc09e8be0517a8afde36fdaea44a7da5b4
Luukuduja <siim_k@outlook.com>
2022-11-07 01:19:43 +0200

    daily-coin tests

--- test/integration/commands/daily-coin.test.ts
+++ test/integration/commands/daily-coin.test.ts
@@ -6,7 +6,7 @@ import { sandbox, testDb } from '../init';
 
 describe('Discord command - /daily-coin', async () => {
     const { execute } = dailyCoin;
-    it.only('Should create new balance for user, if there is none', async () => {
+    it('Should create new balance for user, if there is none', async () => {
         const interaction = getTestInteraction();
         const spy = sandbox.spy(interaction, 'reply');
 
@@ -24,10 +24,50 @@ describe('Discord command - /daily-coin', async () => {
     it('Should update users balance if there is no previous record of using the command', async () => {
         const interaction = getTestInteraction();
         const balance = await createUserBalance(getTestBalanceTemplate({ amount: 100 }));
+        const spy = sandbox.spy(interaction, 'reply');
 
         await execute(interaction);
 
+        expect(spy.calledOnce).to.eq(true);
+        expect(spy.args[0][0]).to.deep.equal({
+            content: `Väike Muum viskas su münditopsi 10 muumimünti. Tule homme tagasi!`,
+            ephemeral: true,
+        });
         const balances = await testDb('balance').whereNot({ amount: 100 });
+        expect(balances.length).to.eq(1);
+    });
+    it('Should update users balance if more than 24 hours has passed since last use of /daily-coin', async () => {
+        const interaction = getTestInteraction();
+        await createUserBalance(
+            getTestBalanceTemplate({ amount: 100, dailyCoin: new Date('2021-11-06T15:10:47.229Z') }),
+        );
+        const spy = sandbox.spy(interaction, 'reply');
+
+        await execute(interaction);
+
+        expect(spy.calledOnce).to.eq(true);
+        expect(spy.args[0][0]).to.deep.equal({
+            content: `Väike Muum viskas su münditopsi 10 muumimünti. Tule homme tagasi!`,
+            ephemeral: true,
+        });
+        const balances = await testDb('balance').whereNot({ amount: 100 });
+        expect(balances.length).to.eq(1);
+    });
+    it('Should update users balance if less than 24 hours has passed since last use of /daily-coin', async () => {
+        const interaction = getTestInteraction();
+        await createUserBalance(
+            getTestBalanceTemplate({ amount: 100, dailyCoin: new Date('2022-11-06T15:10:47.229Z') }),
+        );
+        const spy = sandbox.spy(interaction, 'reply');
+
+        await execute(interaction);
+
+        expect(spy.calledOnce).to.eq(true);
+        expect(spy.args[0][0]).to.deep.equal({
+            content: `Raputad oma münditopsi, aga ei kõlise. Tule proovi hiljem uuesti!`,
+            ephemeral: true,
+        });
+        const balances = await testDb('balance').where({ amount: 100 });
         expect(balances.length).to.eq(1);
     });
 });
