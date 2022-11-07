005659ffda38d2c1f2af2842760882762af0086a
Luukuduja <siim_k@outlook.com>
2022-11-07 02:20:59 +0200

    typo

--- test/integration/commands/daily-coin.test.ts
+++ test/integration/commands/daily-coin.test.ts
@@ -53,7 +53,7 @@ describe('Discord command - /daily-coin', async () => {
         const balances = await testDb('balance').whereNot({ amount: 100 });
         expect(balances.length).to.eq(1);
     });
-    it('Should update users balance if less than 24 hours has passed since last use of /daily-coin', async () => {
+    it('Should not update users balance if less than 24 hours has passed since last use of /daily-coin', async () => {
         const interaction = getTestInteraction();
         await createUserBalance(
             getTestBalanceTemplate({ amount: 100, dailyCoin: new Date('2022-11-06T15:10:47.229Z') }),
