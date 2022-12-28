32cfbd0161e267fbbcf257eea75aff0c6d462d82
Mihkel Kuusküll <mihkel.kuuskull@gmail.com>
2022-12-28 18:48:05 +0200

    need to drop game meta table aswell with down migration

--- src/database/migrations/20221228135132_users.ts
+++ src/database/migrations/20221228135132_users.ts
@@ -30,4 +30,5 @@ export async function up(knex: Knex): Promise<void> {
 
 export async function down(knex: Knex): Promise<void> {
     await knex.schema.dropTable('users');
+    await knex.schema.dropTable('game_meta');
 }
