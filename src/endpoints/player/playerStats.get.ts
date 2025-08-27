c797e00bcfd6c348bb0fd0ba9d3dc3094877207a
Mihkel Kuusküll <mihkel.kuuskull@gmail.com>
2025-08-27 16:07:30 +0300

    comment out for lint

--- src/endpoints/player/playerStats.get.ts
+++ src/endpoints/player/playerStats.get.ts
@@ -1,12 +1,12 @@
-import { findTrackedPlayer } from '../../database/queries/player.query';
+/* import { findTrackedPlayer } from '../../database/queries/player.query';
 
 interface PlayerRankedEntries {
     rankedFlex: object;
     rankedSolo: object;
-}
+} */
 //TODO when player hasnt completed placements API returns empty [], getRecentMatches ja sealt filtreerida W/R kui placementid ja loogika
 export async function handler() {
-    const player = await findTrackedPlayer();
+    //const player = await findTrackedPlayer();
     /* const playerRankedEntries: PlayerRankedEntries = await getRiotUserRankedEntries(player.summonerId);
     return playerRankedEntries; */
 }
